88b5143cab3826eae312499ad4e68623355166d3
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-30 20:32:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song
+attr_accessor :name, :artist, :genre
+
+def initialize(name, artist, genre)
+@name = name
+@artist = artist
+@genre = genre
+@@artist_count = {}
+
+end
+
+  @@count = ["lucifer", "Jay-Z", "rap"]
+  
+  def self.count
+    @@count.count
+end
+  
+  @@artists = ["Jay-Z", "Jay-Z", "Brittany Spears"]
+
+  def self.artists
+@@artists
+  end
+  
+  @@genres = ["rap", "rap", "pop"]
+
+  def self.genres
+  @@genres
+  end
+  
+  @@genre_count = {} 
+  def self.genre_count
+   
+   @@genre_count["key"] = []
+   @@genre_count["key"] << @@genres
+  end
+  
+  
+  def self.artist_count
+    @@artist_count = @@artists.count
+  end
+  end
+
+
+
+lucifer = Song.new("Lucifer", "Jay-Z", "rap")
+Song.count
+Song.artists
+Song.genres
+Song.genre_count
+#Song.artist_count
